97faecbc279523871bf4d29edc529763d6f8dba2
Jade <jadedutralopes@gmail.com>
2017-12-16 05:59:57 -0200

    Search incompleto, Main incompleto

--- Final_Prog2/main.c
+++ Final_Prog2/main.c
@@ -1,3 +1,11 @@
+/******************************************************
+ * Instituto Federal de Santa Catarina
+ * Curso:       Engenharia Eletrônica
+ * Disciplina:  Programação de Computadores II
+ * Professor:   Renan Augusto Starke
+ * Aluna:       Jade Dutra Lopes e Raquel Dutra Kotzias
+ ******************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "lista_enc.h"
@@ -7,9 +15,12 @@
 
 int main()
 {
-    no_t* no;
-    lista_enc_t *lista;
-    arvore_t* tree_title = tree_create(title_comp, title_search_comp);
+    char nome[80];
+    int ano;
+    no_t * no;
+    lista_enc_t * lista;
+    fila_t * fila;
+    arvore_t * tree_title = tree_create(title_comp, title_search_comp);
 
     lista = ler_arquivo("movies.csv");
 
@@ -22,6 +33,54 @@ int main()
 
     //print_list_movies(lista, 10);
 
+
+    while(1) {
+#ifdef _WIN32
+        system("cls");
+#else
+        system("clear");
+#endif
+        printf ("\tMENU PRINCIPAL\n"
+                "\n"
+                "Digite:\n"
+                "<1> Para realizar busca por nome do filme;\n"
+                "<2> Para realizar busca pelo ano;\n"
+                "<3> Para sair.\n");
+        fflush(stdin);
+        scanf("%d",&input);
+
+        if(input == 3)
+            break;
+
+        switch(input) {
+
+        case 1:
+#ifdef _WIN32
+            system("cls");
+#else
+            system("clear");
+#endif
+            printf("Digite as primeiras palavras do título do filme desejado: ");
+            scanf("%s", nome);
+
+            fila = search_tree(nome, tree_title);
+
+
+            break;
+
+        case 2:
+#ifdef _WIN32
+            system("cls");
+#else
+            system("clear");
+#endif
+            printf("Digite o ano do filme desejado: ");
+            scanf("%d", ano);
+
+            fila = search_tree(ano, tree_title);
+
+            break;
+        }
 
     return 0;
 }
